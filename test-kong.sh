2b7993bf3e213c101cf637d3ede8a4060f6116be
Max Griffiths <maxamg@gmail.com>
2017-11-21 18:02:37 -0800

    Max | remove tracing, add back curl output

--- test-kong.sh
+++ test-kong.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -x
-
 ADMIN_SSL_ELB=`kubectl get svc/kong-admin-ssl -o jsonpath="{.status.loadBalancer.ingress[0].hostname}"`
 echo "Hit kong-admin-ssl endpoint"
 TIME=0
@@ -8,7 +6,7 @@ while [[ $TIME -ne ${TIMEOUT:=300} ]]; do
 	sleep ${INTERVAL:=10}
 	((TIME+=$INTERVAL))
 	echo "TIME(/$TIMEOUT): $TIME"
-	OUTPUT=$(curl -I -k https://$ADMIN_SSL_ELB:8444 2>&1)
+	curl -I -k https://$ADMIN_SSL_ELB:8444 2>&1
 	RC=$?
 	if [[ $RC -eq 0 ]]; then
     echo "Kong Admin endpoint returned 200"
